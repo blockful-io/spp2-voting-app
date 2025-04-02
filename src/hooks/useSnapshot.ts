52e43e0de7aa520ec01fec7224efa35a10a23afe
eduramme <eduramme@gmail.com>
2025-04-02 11:02:05 -0300

    fix: document not defined

--- src/hooks/useSnapshot.ts
+++ src/hooks/useSnapshot.ts
@@ -1,9 +1,10 @@
-import { Proposal as SSProposal } from '@snapshot-labs/snapshot.js/dist/src/sign/types';
-import snapshot from '@snapshot-labs/snapshot.js';
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
-import { Web3Provider } from '@ethersproject/providers';
+import { Proposal as SSProposal } from "@snapshot-labs/snapshot.js/dist/src/sign/types";
+import snapshot from "@snapshot-labs/snapshot.js";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { Web3Provider } from "@ethersproject/providers";
 
-const PROPOSAL_ID = "0x8ccd12442eb55df65eb24c7a292eab4a5988d6cc2699484791c07859eb954784"
+const PROPOSAL_ID =
+  "0x8ccd12442eb55df65eb24c7a292eab4a5988d6cc2699484791c07859eb954784";
 
 export type SnapshotVotes = {
   proposal: {
@@ -12,11 +13,9 @@ export type SnapshotVotes = {
   };
   choice: number[];
 };
-;
-
 export interface Candidate {
   id: number;
-  name: string
+  name: string;
   basicBudget: number;
   extendedBudget: number;
   score: number;
@@ -68,16 +67,20 @@ const mockData = [
     basicBudget: 200,
     extendedBudget: 900,
     streamDuration: "FALSE",
-  }
-]
+  },
+];
 
 export function useGetProposals() {
-
   // const queryClient = useQueryClient()
   // const [currentScore, setCurrentScore] = useState(0)
 
-  const { data: proposal, isLoading, isError, isFetching } = useQuery({
-    queryKey: ['snapshot-score'],
+  const {
+    data: proposal,
+    isLoading,
+    isError,
+    isFetching,
+  } = useQuery({
+    queryKey: ["snapshot-score"],
     queryFn: () => fetchProposals(),
   });
 
@@ -108,8 +111,8 @@ export function useGetProposals() {
   // }
 
   async function fetchProposals(): Promise<Proposal | undefined> {
-    const res = await fetch('https://hub.snapshot.org/graphql', {
-      method: 'POST',
+    const res = await fetch("https://hub.snapshot.org/graphql", {
+      method: "POST",
       body: JSON.stringify({
         query: `
           query Proposal($proposalId: String!) {
@@ -123,34 +126,37 @@ export function useGetProposals() {
               scores_total
             }
           }
-        `, variables: { proposalId: PROPOSAL_ID }
+        `,
+        variables: { proposalId: PROPOSAL_ID },
       }),
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-    })
-    const { data: { proposal } } = await res.json()
-
+    });
+    const {
+      data: { proposal },
+    } = await res.json();
 
     return {
       ...proposal,
-      raking: proposal.scores.map((score: number, index: number) => {
-        const additionalData = mockData[index] || {}
-        return {
-          id: index + 1,
-          name: proposal.choices[index],
-          basicBudget: additionalData.basicBudget,
-          extendedBudget: additionalData.extendedBudget,
-          streamDuration: additionalData.streamDuration,
-          score,
-          isBasicApproved: index % 2 === 0,
-          isExtendedApproved: index % 2 === 0,
-        }
-      })
+      raking: proposal.scores
+        .map((score: number, index: number) => {
+          const additionalData = mockData[index] || {};
+          return {
+            id: index + 1,
+            name: proposal.choices[index],
+            basicBudget: additionalData.basicBudget,
+            extendedBudget: additionalData.extendedBudget,
+            streamDuration: additionalData.streamDuration,
+            score,
+            isBasicApproved: index % 2 === 0,
+            isExtendedApproved: index % 2 === 0,
+          };
+        })
         .sort((a: Candidate, b: Candidate) => {
-          if (a.isExtendedApproved) return -1
-          if (b.isBasicApproved) return 1
-          return b.score - a.score
+          if (a.isExtendedApproved) return -1;
+          if (b.isBasicApproved) return 1;
+          return b.score - a.score;
         }),
     };
   }
@@ -160,37 +166,43 @@ export function useGetProposals() {
     isLoading,
     isError,
     isFetching,
-  }
-
+  };
 }
 
 export function useVoteOnProposals() {
-  const queryClient = useQueryClient()
+  const queryClient = useQueryClient();
 
-  const client = new snapshot.Client712('https://hub.snapshot.org');
-  const web3 = new Web3Provider(window.ethereum!);
+  const client = new snapshot.Client712("https://hub.snapshot.org");
+  const web3 =
+    typeof window !== "undefined" ? new Web3Provider(window.ethereum!) : null;
 
-  const { mutateAsync: voteFunc, error, isPending } = useMutation({
+  const {
+    mutateAsync: voteFunc,
+    error,
+    isPending,
+  } = useMutation({
     mutationFn: voteOnProposal,
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['snapshot-score'] })
-    }
+      queryClient.invalidateQueries({ queryKey: ["snapshot-score"] });
+    },
   });
 
   async function voteOnProposal(choice: number[]) {
+    if (!web3) {
+      throw new Error("Web3 provider not available");
+    }
     const [account] = await web3.listAccounts();
     await client.vote(web3, account, {
-      space: 'pikonha.eth',
+      space: "pikonha.eth",
       proposal: PROPOSAL_ID,
       type: "ranked-choice",
       choice,
-    })
+    });
   }
 
   return {
     voteFunc,
     error,
-    isPending
-  }
-
+    isPending,
+  };
 }
